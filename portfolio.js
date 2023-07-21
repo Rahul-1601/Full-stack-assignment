efe14aac2b1a34f1b10df7ea2c50ab9ce4a95342
Rahul-1601 <79655228+Rahul-1601@users.noreply.github.com>
2023-07-21 12:47:16 +0530

    portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
